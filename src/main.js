72c6b034ab296eb2a41a52f0165ac9c3f498c842
Maks12-droid <127047393+Maks12-droid@users.noreply.github.com>
2024-04-04 11:53:38 +0300

    1

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { renderImgs } from './js/render-functions';
-import { fetchImg } from './js/pixabay-api.js'; 
+import { fetchImg } from './js/pixabay-api';
 
 export const setGallery = document.querySelector('ul.gallery');
 export let imgset;
@@ -14,75 +16,75 @@ export let addPage = 1;
 
 const inputfield = document.querySelector('input');
 const fillForm = document.querySelector('form');
+const addImgs = document.querySelector('#addImg');
+
 const preloader = document.querySelector('.preloader');
-const gallery = document.querySelector('.gallery');
 
 const showLoader = () => {
   preloader.style.display = 'flex';
 };
-
 const hideLoader = () => {
   preloader.style.display = 'none';
 };
-
 const handleLoad = () => {
   document.body.classList.add('loaded');
   document.body.classList.remove('loaded_hiding');
 };
 
-window.onload = handleLoad;
-
-// Ініціалізація SimpleLightbox
-let lightbox = new SimpleLightbox('.gallery a');
-
-fillForm.addEventListener('submit', event => {
+fillForm.addEventListener('submit', async event => {
   event.preventDefault();
-  const input = inputfield.value.trim();
-  if (input !== '') {
-    showLoader(); 
-    fetchImg(input)
-      .then((photos) => {
-        if (photos.hits.length > 0) {
-          const galleryHTML = photos.hits.map(photo => `<a href="${photo.largeImageURL}"><img src="${photo.webformatURL}" alt="${photo.tags}" /></a>`).join('');
-          gallery.innerHTML = galleryHTML;
-          lightbox.refresh();
-        } else {
-          gallery.innerHTML = '';
-          iziToast.show({
-            message: 'No images found!',
-            theme: 'dark',
-            progressBarColor: '#FFFFFF',
-            color: '#EF4040',
-            position: 'topRight',
-          });
-        }
-        hideLoader(); 
-      })
-      .catch((error) => {
-        console.error(error);
-        hideLoader(); 
-        iziToast.error({
-          message: 'Sorry, an error occurred while loading. Please try again!',
-          theme: 'dark',
-          progressBarColor: '#FFFFFF',
-          color: '#EF4040',
-          position: 'topRight',
-        });
-      });
-    fillForm.reset();
-  } else {
-    iziToast.show({
-      message: 'Please complete the field!',
-      theme: 'dark',
-      progressBarColor: '#FFFFFF',
-      color: '#EF4040',
+  addPage = 1;
+  imgset = {};
+  searchImgs = inputfield.value.trim();
+
+  if (!searchImgs.length) {
+    iziToast.error({
+      color: 'yellow',
+      message: ` Please fill in the field for search images.`,
       position: 'topRight',
     });
+    setGallery.innerHTML = '';
+    return;
+  }
+
+  showLoader();
+
+  try {
+    imgset = await fetchImg();
+
+    if (!imgset.length) {
+      iziToast.error({
+        color: 'red',
+        message: `❌ Sorry, there are no images matching your search query. Please try again!`,
+        position: 'topRight',
+      });
+      addImgs.style.display = 'none';
+      return;
+    }
+
+    if (perPage <= imgset.length) {
+      addImgs.style.display = 'flex';
+    } else {
+      iziToast.error({
+        color: 'blue',
+        message: `We're sorry, but you've reached the end of search results.`,
+        position: 'topRight',
+      });
+    }
+    renderImgs(imgset);
+    scroll();
+  } catch (error) {
+    iziToast.error({
+      color: 'red',
+      message: `❌ Sorry, there are no images matching your search query. Please try again!`,
+      position: 'topRight',
+    });
+  } finally {
+    hideLoader();
+    handleLoad();
   }
 });
 
-export { searchImgs };
-    
 addImgs.addEventListener('click', async event => {
   event.preventDefault();
 
@@ -117,3 +119,12 @@ addImgs.addEventListener('click', async event => {
   }
 });
 window.onload = handleLoad;
+
+async function scroll() {
+  const imgSize = document.querySelector('.img-blok');
+  const domRect = imgSize.getBoundingClientRect();
+  window.scrollBy({
+    top: domRect.height * 2,
+    behavior: 'smooth',
+  });
+}
